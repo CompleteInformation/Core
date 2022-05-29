87a5d7b6d78bf30ea71f570be066a091d1382c75
NicoVIII <nico@nicoviii.net>
2022-05-29 14:36:38 +0000

    Move a few things into frontend lib

--- src/base/frontend/web/Library.fs
+++ src/base/frontend/web/Library.fs
@@ -1,4 +1,17 @@
 ﻿namespace CompleteInformation.Base.Frontend.Web
 
-module test =
-    let x = "y"
+open Elmish
+open Elmish.React
+open Fable.Remoting.Client
+
+[<RequireQualifiedAccess>]
+module Api =
+    let buildProxy<'Api> () =
+        Remoting.createApi ()
+        |> Remoting.withBaseUrl "http://localhost:8084/api"
+        |> Remoting.buildProxy<'Api>
+
+[<RequireQualifiedAccess>]
+module Program =
+    let mount program =
+        Program.withReactSynchronous "module-slot" program
